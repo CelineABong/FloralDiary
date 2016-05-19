78b75da45fce5af3a1f9f4fa3bd75e5410d3120a
CelBHL <celinebonghl3@gmail.com>
2016-05-18 21:12:47 -0700

    Modified floraldiary.sql

--- floraldiary.sql
+++ floraldiary.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 18, 2016 at 08:01 AM
+-- Generation Time: May 19, 2016 at 06:12 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -44,19 +44,24 @@ CREATE TABLE `image` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
   `path` varchar(255) NOT NULL,
   `userid` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `image`
 --
 
-INSERT INTO `image` (`id`, `title`, `description`, `path`, `userid`) VALUES
-(32, 'asf', 'asdfasdf', './model/image/celinebong/1463354052.jpg', 12),
-(33, 'Screenshot', 'Print screen', './model/image/mumu/1463355964.jpg', 17),
-(34, 'Hey', 'asfdasfasf', './model/image/mumuu/1463358874.jpg', 17),
-(35, 'sdafasdf', 'asdfasf', './model/image/fifi/1463543692.jpg', 16);
+INSERT INTO `image` (`id`, `title`, `description`, `price`, `path`, `userid`) VALUES
+(32, 'asf', 'asdfasdf', 0, './model/image/celinebong/1463354052.jpg', 12),
+(33, 'Screenshot', 'Print screen', 0, './model/image/mumu/1463355964.jpg', 17),
+(34, 'Hey', 'asfdasfasf', 0, './model/image/mumuu/1463358874.jpg', 17),
+(35, 'sdafasdf', 'asdfasf', 0, './model/image/fifi/1463543692.jpg', 16),
+(36, 'weqr', 'qwer', 12, './model/image/fifi/1463630250.jpg', 16),
+(37, 'ree', 'ree', 11, './model/image/fifi/1463630267.jpg', 16),
+(38, 'heyy', 'yheyy', 33, './model/image/fifi/1463630485.jpg', 16),
+(39, 'test', 'test', 335, './model/image/fifi/1463630734.jpg', 16);
 
 -- --------------------------------------------------------
 
@@ -76,7 +81,8 @@ CREATE TABLE `likes` (
 INSERT INTO `likes` (`user_id`, `image_id`) VALUES
 (16, 32),
 (16, 34),
-(16, 35);
+(16, 35),
+(16, 38);
 
 -- --------------------------------------------------------
 
@@ -144,7 +150,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `image`
 --
 ALTER TABLE `image`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=40;
 --
 -- AUTO_INCREMENT for table `users`
 --
